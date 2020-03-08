8999f0c4ac75a65febf798b457272dba095c0d15
Barry <semieng94@gmail.com>
2020-03-07 21:59:47 -0600

    ufo-1 working

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,5 +1,7 @@
-const tableData = data
+
 const tbody = d3.select('tbody')
+const input = d3.select("input")
+const button = d3.select("button")
 
 function buildTable(data) {
     tbody.html('')  // Clear existing data
@@ -16,22 +18,23 @@ function buildTable(data) {
 const handleClick = () => {
     d3.event.preventDefault()
 
-    const date = d3.select('#datetime').property('value')
-    let filteredData = tableData;
+    let filterCond = input.property("value")
 
-    if (date)
-        filteredData = filteredData.filter(row => row.datetime === date)
     
     tbody.html('')  // Clear existing data
-
-    data.forEach(row => {
-        const currentRow = tbody.append('tr')  // append row
-        Object.values(row).forEach(value => {
-            const cell = currentRow.append('td')
-            cell.text(value)
-        })
+    data.filter(dataRow => dataRow.datetime === filterCond).forEach(dataRow => {
+        let row = tbody.append("tr")
+        // row.append("td").text(dataRow.)
+        row.append("td").text(dataRow.datetime)
+        row.append("td").text(dataRow.city)
+        row.append("td").text(dataRow.state)
+        row.append("td").text(dataRow.country)
+        row.append("td").text(dataRow.shape)
+        row.append("td").text(dataRow.durationMinutes)
+        row.append("td").text(dataRow.comments)
     })
 }
 
-d3.selectAll('#filter-btn').on('click', handleClick)
+button.on("click",handleClick)
+input.on("change", handleClick)
 // buildTable(tableData)
